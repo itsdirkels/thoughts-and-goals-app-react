3d0cd56a73d9b592fd00d3d6247800632bd2ae0b
Daniel Wessels <34720459+itsdirkels@users.noreply.github.com>
2024-04-21 22:42:49 +0200

    Trying to fix 1

--- src/components/OutputContainer.jsx
+++ src/components/OutputContainer.jsx
@@ -5,7 +5,7 @@ const OutputContainer = ({children}) => {
     <div className='mt-2 mb-0 box-border w-11/12 bg-gradient-to-b 
     from-green-500 to-green-300 border-white border-4
     min-h-[20rem] h-[40%] shadow-inner p-5
-    flex flex-row items-stretch justify-evenly m-10 s:mb-4 s:overflow-y-scroll'>
+    flex flex-row items-stretch justify-evenly m-10 s:mb-4'>
         {children}
     </div>
   )
